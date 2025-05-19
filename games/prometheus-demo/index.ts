cc52f1274c4f96dfbbee7b879f9340066503ad02
Gerard Cuello <fontinixxl@users.noreply.github.com>
2025-05-19 17:37:00 +0200

    chore: remove tracked folder (games/*) now ignored by git

--- games/prometheus-demo/index.ts
+++
@@ -1,8 +0,0 @@
-import { init } from './app';
-
-const container = document.getElementById('app');
-if (container) {
-  init(container);
-} else {
-  console.warn('No #app element found!');
-}
--- games/prometheus-demo/test-import.js
+++
@@ -1,4 +0,0 @@
-// Try direct import path
-import { Engine } from '../../packages/engine-core/dist/index.js';
-
-console.log('Engine imported successfully:', Engine);
